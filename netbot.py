e12459f0f89ea79bb42db3d92aaa6055ded71eea
Paul Philion <pphlilion@istreamplanet.com>
2023-08-23 19:24:40 -0700

    Got /site working.

--- netbot.py
+++ netbot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-## --log=DEBUG
 
 import os
 import logging
@@ -13,23 +12,87 @@ import pynetbox
 from discord.commands import option
 from dotenv import load_dotenv
 
+from discord.ext import commands
+
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
-NETBOX = None
 
 log.debug('initializing bot')
-bot = discord.Bot()
 
-# from https://drive.google.com/drive/u/1/folders/1zfSaL_Whbi8esFZk8tb8EKsdcUhGNDDo
-#SITES = [
-#    "filipino-community-village",
-#    "katharines-place",
-#    "lihi-southend-village",
-#    "nickelsville-cd",
-#    "nickelsville-northlake",
-#    "progressive-skyway-village",
-#]
+# load security creds from the `.env` file
+load_dotenv()
 
-SITES = []
+
+class NetBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("!"), intents=intents
+        )
+
+    def run(self):
+        log.info(f"starting {self}")
+        super().run(os.getenv('DISCORD_TOKEN'))
+
+    async def on_ready(self):
+        log.info(f"Logged in as {self.user} (ID: {self.user.id})")
+
+
+class Netbox():
+    def __init__(self):
+        # build the client and initalze the constructs
+        self.client = Netbox.build_client(url=os.getenv('NETBOX_URL'), token=os.getenv('NETBOX_TOKEN'))
+        self.reload()
+
+    # load any data that needs to be loaded
+    def reload(self):
+        """Load the data and cache locally, to avoid a lot of slow network calls"""
+        log.info("Loading sites")
+        self.sites = self.load_sites()
+        self.site_names = self.sites.keys()
+        log.info(f"Loaded: {self.site_names}")
+
+    # build a client for communicating with an instance of netbox
+    def build_client(url:str, token:str):
+        """Given a secure token, build a client to communicate with a netbox instance"""
+        client = pynetbox.api(url, token)
+
+        # create a custom session to disable SSL validation, as per
+        # https://pynetbox.readthedocs.io/en/latest/advanced.html#ssl-verification
+        # FIXME This should be removed as soon as real certs are in place.
+        session = requests.Session()
+        session.verify = False
+        client.http_session = session
+        
+        return client
+    
+    def site_names(self):
+        return self.site_names
+
+    def site(self, slug:str):
+        return self.sites[slug]
+
+    def load_sites(self):
+        """Load the list of sites from the netbox client"""
+        sites = {}
+        for site in self.client.dcim.sites.all():
+            sites[site.slug] = site
+            
+        return sites
+
+
+
+# utility stuff for formatting
+def format_site(site):
+    #return f"**({site.url})[{site.slug}]** {site.name} {site.last_updated}"
+    url = site.url.replace('/api', '') # strip the /api from the url
+    return f"**[{site.slug}]({url})** - {site.name}"
+
+
+netbox = Netbox()
+bot = NetBot()
+
 
 ## Left as example code, for now
 #async def color_searcher(ctx: discord.AutocompleteContext):
@@ -47,61 +110,45 @@ SITES = []
 #    ]
 
 
-def build_netbox_client(url, token):
-    """Given a secure token, build a client to communicate with a netbox instance"""
-    client = pynetbox.api(url, token)
-
+#def build_netbox_client(url, token):
+#    """Given a secure token, build a client to communicate with a netbox instance"""
+#    client = pynetbox.api(url, token)
+#
     # create a custom session to disable SSL validation, as per
     # https://pynetbox.readthedocs.io/en/latest/advanced.html#ssl-verification
     # FIXME This should be removed as soon as real certs are in place.
-    session = requests.Session()
-    session.verify = False
-    client.http_session = session
-
-    return client
-
-
-def load_sites(netbox):
-    """Load the list of sites from the netbox client"""
-    sites = {}
-    for site in netbox.dcim.sites.all():
-        #sites.append(site.slug) # just grabbing the 'slug', or url short name.
-        sites[site.slug] = site.id
-    return sites
+#    session = requests.Session()
+#    session.verify = False
+#    client.http_session = session
+#
+#    return client
 
 
-async def get_sites(ctx: discord.AutocompleteContext):
-    """Returns a list of colors that begin with the characters entered so far."""
-    return [site for site in SITES if site.startswith(ctx.value.lower())]
-
-@bot.slash_command(name="list")
-async def sites(ctx: discord.AutocompleteContext):
-    msg = ""
-    for site in NETBOX.dcim.sites.all():
-        msg += format(site) + '\n'
-
-    log.warn
-    await ctx.respond(msg)
+async def complete_sites(ctx: discord.AutocompleteContext):
+    """Returns a list of sites that begin with the characters entered so far."""
+    names = netbox.site_names()
+    return [site for site in names if site.startswith(ctx.value.lower())]
 
 
-def format_site(netbox, site_str):
-    return format(netbox.dcim.sites.get(slug=site_str))
-
-
-def format(site):
-    #return f"**({site.url})[{site.slug}]** {site.name} {site.last_updated}"
-    return f"({site.url})[{site.name}]"
+#@bot.slash_command(name="sites", description="list all the active sites")
+#async def sites(ctx):
+#    msg = ""
+#    for site in netbox.sites():
+#        msg += format(site) + '\n'
+#    await ctx.respond(msg)
 
 
 @bot.slash_command(name="site") 
-@option("site", description="pick a site", autocomplete=get_sites)
-#@option("animal", description="Pick an animal!", autocomplete=get_animals)
-async def autocomplete_example(
-    ctx: discord.ApplicationContext,
-    site_slug: str,
-):
-    site_msg = format_site(NETBOX, site_slug) ### FIXME: global NETBOX defination
-    await ctx.respond(site_msg)
+@option("site", description="pick a site", autocomplete=complete_sites)
+async def site_command(ctx: discord.ApplicationContext, site="all"):
+    msg = ""
+    if site == 'all':
+        for _site in netbox.sites.values():
+            msg += format_site(_site) + '\n'
+    else:
+        msg = format_site(netbox.site(site))
+        
+    await ctx.respond(msg)
 
 
 #@bot.slash_command(name="site")
@@ -118,19 +165,6 @@ async def autocomplete_example(
 #    await ctx.respond(site_msg)
 
 
-# load security creds from the `.env` file
-load_dotenv()
-discord_token = os.getenv('DISCORD_TOKEN')
-netbox_token = os.getenv('NETBOX_TOKEN')
-netbox_url = os.getenv('NETBOX_URL')
 
-# load some cached settings from netbox
-# TODO: need a hook to refresh this cache: command in the bot (eventually)
-log.info("loading sites from netbox")
-NETBOX = build_netbox_client(netbox_url, netbox_token)
-SITES.extend(load_sites(NETBOX))
-log.debug(SITES)
-
-# run the bot, main thread
-log.info("starting bot")
-bot.run(discord_token)
+# run the bot
+bot.run()
