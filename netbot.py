e13d76e37e1ec0a635a58251b658ac1b0aca4f86
Paul Philion <pphlilion@istreamplanet.com>
2023-11-06 11:57:26 -0800

    Updated redmine and got the standard 'tickets' kanban board working.

--- netbot.py
+++ netbot.py
@@ -7,7 +7,7 @@ import logging
 import discord
 
 import redmine
-import netbox
+#import netbox
 
 from discord.commands import option
 from dotenv import load_dotenv
@@ -36,35 +36,40 @@ class NetBot(commands.Bot):
         log.info(f"Logged in as {self.user} (ID: {self.user.id})")
 
 
-netbox_client = netbox.Client()
-redmine_client = redmine.Client()
+log.info(f"initializing {__name__}")
+load_dotenv()
+#netbox_client = netbox.Client()
+client = redmine.Client()
 bot = NetBot()
 
 
-async def complete_sites(ctx: discord.AutocompleteContext):
-    """Returns a list of sites that begin with the characters entered so far."""
-    names = netbox_client.site_names
-    return [site for site in names if site.startswith(ctx.value.lower())]
+#async def complete_sites(ctx: discord.AutocompleteContext):
+#    """Returns a list of sites that begin with the characters entered so far."""
+#    names = netbox_client.site_names
+#    return [site for site in names if site.startswith(ctx.value.lower())]
 
 
-@bot.slash_command(name="site") 
-@option("site", description="pick a site", autocomplete=complete_sites)
-async def site_command(ctx: discord.ApplicationContext, site="all"):
-    msg = ""
-    if site == 'all':
-        msg = netbox_client.format_sites()
-    else:
-        msg = netbox_client.format_site(netbox_client.site(site))
-        
-    await ctx.respond(msg)
+#@bot.slash_command(name="site") 
+#@option("site", description="pick a site", autocomplete=complete_sites)
+#async def site_command(ctx: discord.ApplicationContext, site="all"):
+#    msg = ""
+#    if site == 'all':
+#        msg = netbox_client.format_sites()
+#    else:
+#        msg = netbox_client.format_site(netbox_client.site(site))
+#        
+#    await ctx.respond(msg)
 
 
 @bot.slash_command(name="tickets")
 async def tickets_command(ctx: discord.ApplicationContext):
     # query issues
-    msg = redmine_client.format_issues()
-    await ctx.respond(msg)
+    tickets = client.find_tickets()
+    log.info(f"found {len(tickets)} tickets")
 
+    for status in ["New", "In Progress", "Resolved"]:
+        msg = client.format_section(tickets, status)
+        await ctx.respond(msg)
 
 # run the bot
 bot.run()
--- redmine.py
+++ redmine.py
@@ -1,37 +1,228 @@
+#!/usr/bin/env python3
+
 import os
+import re
 import json
 import requests
+import logging
 import datetime as dt
-import humanize
+#import humanize
 
+from dotenv import load_dotenv
 from types import SimpleNamespace
-#from collections import namedtuple
 
 
 #from typing import TypedDict # see https://peps.python.org/pep-0589/
-from dataclasses import dataclass
+#from dataclasses import dataclass
+
+#logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 class Client(): ## redmine.Client()
     def __init__(self):
         self.url = os.getenv('REDMINE_URL')
         self.token = os.getenv('REDMINE_TOKEN')
-        self.issue_query = "/issues.json?status_id=open&sort=category:desc,updated_on"
-        # GET /issues.json?status_id=open&sort=category:desc,updated_on
 
-    def load_issues(self):
-        #TODO add query params and default query
-        """load open issues from a redmine instance"""
-        headers = {
-            'User-Agent': 'netbot/0.0.1',
+    def create_ticket(self, user_id, subject, body):
+        # https://www.redmine.org/projects/redmine/wiki/Rest_Issues#Creating-an-issue
+
+        headers = { # TODO DRY headers
+            'User-Agent': 'netbot/0.0.1', # TODO update to project version, and add version management
             'Content-Type': 'application/json',
             'X-Redmine-API-Key': self.token,
+            'X-Redmine-Switch-User': user_id, # Make sure the comment is noted by the correct user
         }
 
-        r = requests.get(f"{self.url}{self.issue_query}", headers=headers)
-        # parse json response in `r.text`
-        result = json.loads(r.text)
-        # but there's an extra 'issues' wrapper
-        return result['issues']
+        data = {
+            'issue': {
+                'project_id': 1,
+                'subject': subject,
+                'description': body,
+            }
+        }
+
+        r = requests.post(
+            url=f"{self.url}/issues.json", 
+            data=json.dumps(data), 
+            headers=headers)
+        
+        print(f"create_ticket response: {r}")
+        
+        # check status
+        #if r.status_code != 201:
+        # check 201 status
+        #root = json.loads(r.text, object_hook= lambda x: SimpleNamespace(**x))
+        #ticket = root.ticket[0]
+        #return ticket
+
+    def append_message(self, ticket_id:str, user_id:str, note:str, attachments):
+        headers = { # TODO DRY headers
+            'User-Agent': 'netbot/0.0.1', # TODO update to project version, and add version management
+            'Content-Type': 'application/json',
+            'X-Redmine-API-Key': self.token,
+            'X-Redmine-Switch-User': user_id, # Make sure the comment is noted by the correct user
+        }
+
+        # PUT a simple JSON structure
+        data = {
+            'issue': {
+                'notes': note,
+                'uploads': []
+            }
+        }
+
+        # add the attachments
+        if len(attachments) > 0:
+            for a in attachments:
+                data['issue']['uploads'].append({
+                    "token": a.token, 
+                    "filename": a.name,
+                    "content_type": a.content_type,
+                })
+
+        r = requests.put(
+            url=f"{self.url}/issues/{ticket_id}.json", 
+            data=json.dumps(data),
+            headers=headers)
+        
+        # check status
+        if r.status_code != 204:
+            root = json.loads(r.text, object_hook= lambda x: SimpleNamespace(**x))
+            log.error(f"append_message, status={r.status_code}: {root}")
+            # throw exception?
+
+
+    def upload_file(self, user_id, data, filename, content_type):
+        # POST /uploads.json?filename=image.png
+        # Content-Type: application/octet-stream
+        # (request body is the file content)
+        
+        headers = { # TODO DRY headers
+            'User-Agent': 'netbot/0.0.1', # TODO update to project version, and add version management
+            'Content-Type': 'application/octet-stream',
+            'X-Redmine-API-Key': self.token,
+            'X-Redmine-Switch-User': user_id, # Make sure the comment is noted by the correct user
+        }
+
+        r = requests.post(
+            url=f"{self.url}/uploads.json?filename={filename}", 
+            files={ 'upload_file': (filename, data, content_type) },
+            headers=headers
+        )
+        
+        # 201 response: {"upload":{"token":"7167.ed1ccdb093229ca1bd0b043618d88743"}}
+        if r.status_code == 201:
+            # all good, get token
+            root = json.loads(r.text, object_hook= lambda x: SimpleNamespace(**x))
+            token = root.upload.token
+            log.info(f"Uploaded {filename} {content_type}, got token={token}")
+            return token
+        else:
+            #print(vars(r))
+            log.error(f"Error uploading {filename} {content_type} - response:{r}")
+            # todo throw exception
+
+    def upload_attachments(self, user_id, attachments):
+        # uploads all the attachments, 
+        # sets the upload token for each 
+        for a in attachments:
+            token = self.upload_file(user_id, a.payload, a.name, a.content_type)
+            a.set_token(token)
+
+    ### OLD REMOVE
+    def xxx_append_attachment(self, ticket_id, user_id, data, filename, content_type):
+        # upload the data as a new file
+        upload_token = self.upload_file(user_id, data, filename, content_type)
+
+        # then PUT the upload to the issue API
+        headers = { # TODO DRY headers
+            'User-Agent': 'netbot/0.0.1', # TODO update to project version, and add version management
+            'Content-Type': 'application/json',
+            'X-Redmine-API-Key': self.token,
+            'X-Redmine-Switch-User': user_id, # Make sure the comment is noted by the correct user
+        }
+
+        # PUT a simple JSON structure with the uploaded file info
+        data = {
+            'issue': {
+                'notes': f"Uploading attachment {filename} to ticket #{ticket_id}.",
+                'uploads': [
+                    { 
+                        "token": upload_token, 
+                        "filename": filename,
+                        "content_type": content_type,
+                    }
+                ]
+            }
+        }
+
+        r = requests.put(
+            url=f"{self.url}/issues/{ticket_id}.json", 
+            data=json.dumps(data),
+            headers=headers)
+
+        print(f"append_attachment response: {r}")
+
+    def find_user(self, email):
+        response = self.query(f"/users.json?name={email}")
+        # TODO better error checking 
+        return response.users[0]
+    
+    def get_ticket(self, ticket_num:int):
+        response = self.query(f"/issues.json?issue_id={ticket_num}")
+        if response.total_count > 0:
+            return response.issues[0]
+        else:
+            log.warning(f"Unknown ticket number: {ticket_num}")
+            return None
+
+    def find_ticket_from_str(self, str:str):
+        # for now, this is a trivial REGEX to match '#nnn' in a string, and return ticket #nnn
+        match = re.search(r'#(\d+)', str)
+        if match:
+            ticket_num = int(match.group(1))
+            return self.get_ticket(ticket_num)
+        else:
+            log.warning(f"Unable to match ticket number in: {str}")
+            return None
+    
+    def create_user(self, email, first, last):
+        user = {
+            'login': email,
+            'firstname': 'test-first',
+            'lastname': 'test-last',
+            'mail': email,
+        }
+        # on create, assign watcher: sender.
+
+    def most_recent_ticket_for(self, email):
+        # get the user record for the email
+        user = self.find_user(email)
+
+        if user:
+            # query open tickets created by user, sorted by most recently updated, limit 1
+            response = self.query(f"/issues.json?author_id={user.id}&status_id=open&sort=updated_on:desc&limit=1")
+
+            if response.total_count > 0:
+                return response.issues[0]
+            else:
+                log.info(f"No recent open ticket found for: {user}")
+                return None
+        else:
+            log.warning(f"Unknown email: {email}")
+            return None
+        
+    # project_id=1&tracker_id=4&sort=priority:desc,updated_on:desc,id:desc
+    def find_tickets(self):
+        # "kanban" query: all ticket open or closed recently
+        project=1
+        tracker=4
+        response = self.query(f"/issues.json?project_id={project}&tracker_id={tracker}\
+                              &status_id=*&sort=priority:desc,updated_on:desc,id:desc&limit=100")
+
+        return response.issues
+
+
 
     def query(self, query_str: str):
         """run a query against a redmine instance"""
@@ -43,80 +234,40 @@ class Client(): ## redmine.Client()
         # run the query with the 
         r = requests.get(f"{self.url}{query_str}", headers=headers)
 
-        root = json.loads(r.text, object_hook= lambda x: SimpleNamespace(**x))
-        #root = json.loads(r.text, object_hook=lambda d: namedtuple('Issue', d.keys())(*d.values())) 
-
-        return root.issues
-
-
-    def format_issue(self, issue):
-        # parse the "updated_on" field to create a datetime
-        last_updated = dt.datetime.strptime(issue['updated_on'], '%Y-%m-%dT%H:%M:%SZ')
-        # create a human-readable time difference
-        age = humanize.naturaldelta(dt.datetime.now() - last_updated)
-        # format everything for 
-        return f"**[#{issue['id']}](<{self.url}/issues/{issue['id']}>)** {issue['subject']} - {issue['priority']['name']} - {age} old"
+        # check 200 status code
+        if r.status_code == 200:
+            return json.loads(r.text, object_hook= lambda x: SimpleNamespace(**x))
+        else:
+            return None
     
-    def format_issues(self):
-        msg = ""
-        for issue in self.load_issues():
-            msg += self.format_issue(issue) + '\n'
-        return msg
 
+    def format_report(self, tickets):
+        # 3 passes: new, in progress, closed
+        
+        print(len(self.format_section(tickets, "New")))
+        print(self.format_section(tickets, "In Progress"))
+        print(self.format_section(tickets, "Resolved"))
 
-# @dataclass
-# class NamedId:
-#     id: int
-#     name: str
+    def format_section(self, tickets, status):
+        section = ""
+        section += f"> {status}\n"
+        #section += f"```\n"
+        for ticket in tickets:
+            if ticket.status.name == status:
+                url = f"{self.url}/issues/{ticket.id}"
+                try: # hack to get around missing key
+                    assigned_to = ticket.assigned_to.name
+                except AttributeError:
+                    assigned_to = ""
+                section += f"[**`{ticket.id:>4}`**]({url})`  {ticket.priority.name:<6}  {ticket.updated_on[:10]}  {assigned_to[:20]:<20}  {ticket.subject}`\n"
+        #section += f"```\n"
+        return section
 
-#     def __str__(self) -> str:
-#         return self.name
+if __name__ == '__main__':
+    # load credentials 
+    load_dotenv()
 
-
-# @dataclass
-# class Issue:
-#     id: int
-#     project: NamedId
-#     tracker: NamedId
-#     status: NamedId
-#     priority: NamedId
-#     author: NamedId
-#     assigned_to: NamedId
-#     subject: str
-#     description: str
-#     start_date: dt.date
-#     due_date: dt.date
-#     done_ratio: float
-#     is_private: bool
-#     estimated_hours: float
-#     total_estimated_hours: float
-#     spent_hours: float
-#     total_spent_hours: float
-#     created_on: dt.datetime
-#     updated_on: dt.datetime
-#     closed_on: dt.datetime
-#     custom_fields: dict
-#     #url: str # TODO is there a formal URL type?
-
-
-### issue fields ###
-# issue url: http://40.65.85.252/issues/22 -> {base_url}/issues/{issue.id}
-# {'id': 2, 'project': {'id': 1, 'name': 'Seattle Community Network'}, 
-# 'tracker': {'id': 3, 'name': 'Support'}, 
-# 'status': {'id': 1, 'name': 'New', 'is_closed': False}, 
-# 'priority': {'id': 3, 'name': 'High'}, 
-# 'author': {'id': 5, 'name': 'Paul Philion'}, 
-# 'assigned_to': {'id': 6, 'name': 'Esther Jang'}, 
-# 'subject': 'Testing email notification', 
-# 'description': 'Is email getting sent?\r\n\r\nEsther will know.', 
-# 'start_date': '2023-08-01', 
-# 'due_date': None, 
-# 'done_ratio': 0, 
-# 'is_private': False, 
-# 'estimated_hours': None, 
-# 'total_estimated_hours': None, 
-# 'spent_hours': 0.0, 
-# 'total_spent_hours': 0.0, 
-# 'created_on': '2023-08-01T00:21:07Z', 
-# 'updated_on': '2023-08-04T16:34:24Z', 
-# 'closed_on': None
+    # construct the client and run the email check
+    client = Client()
+    tickets = client.find_tickets()
+    client.format_report(tickets)
